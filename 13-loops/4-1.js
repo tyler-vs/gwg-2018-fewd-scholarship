9f21213e65a19a0c4f0c91b94005565ff1c3151a
tyler-vs <tylervucf@knights.ucf.edu>
2018-01-23 17:26:01 -0500

    tests: add solution to 4-1.js

--- 13-loops/4-1.js
+++ 13-loops/4-1.js
@@ -4,8 +4,26 @@
 
 var x = 1;
 
-while (/* your stop condition goes here */) {
+while (x <= 20) {
+
+    console.log("X is at: " + x + ".");
+
     // check divisibility
+
+    if ((x % 3 === 0) && (x % 5 === 0)) {
+      console.log("JuliaJames");
+
+    } else if (x % 3 === 0) {
+      console.log("Julia");
+
+    } else if (x % 5 === 0) {
+      console.log("James");
+
+    } else {
+      console.log(x);
+    }
     // print Julia, James, or JuliaJames
+
     // increment x
+    x++;
 }
