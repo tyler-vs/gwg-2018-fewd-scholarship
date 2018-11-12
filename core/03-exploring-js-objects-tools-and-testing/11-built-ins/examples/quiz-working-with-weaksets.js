95d08e21522945db5bd3c9f13a94e0e2cfa25855
tyler-vs <tylervucf@knights.ucf.edu>
2018-11-11 20:18:19 -0500

    docs: Add quiz from lesson with solution.

---
+++ core/03-exploring-js-objects-tools-and-testing/11-built-ins/examples/quiz-working-with-weaksets.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Using Sets (3-2)
+ *
+ * Create the following variables:
+ *     - uniqueFlavors and set it to a new WeakSet object
+ *     - flavor1 and set it equal to `{ flavor: 'chocolate' }`
+ *     - flavor2 and set it equal to an object with property 'flavor' and value of your choice!
+ *
+ * Use the `.add()` method to add the objects `flavor1` and `flavor2` to `uniqueFlavors`
+ * Use the `.add()` method to add the `flavor1` object (again!) to the `uniqueFlavors` set
+ */
+
